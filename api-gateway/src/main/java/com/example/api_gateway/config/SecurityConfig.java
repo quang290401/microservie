60729ada64eeb2876c36aca2160a363b5323dd6d
Lê Minh Quang <quanglm042000@gmail.com>
2024-12-10 10:46:39 +0700

    Update

--- api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -9,11 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-
-
 
 @Configuration
 @EnableWebFluxSecurity
